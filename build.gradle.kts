662b180eb4def556f952c2f88dfd083d4b9236cf
YatesRocks <yates.j.a@outlook.com>
2024-02-15 14:11:32 -0500

    Add dependencies and application plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "org.example"
@@ -10,10 +11,29 @@ repositories {
 }
 
 dependencies {
+    // application plugin
+    implementation("com.google.guava:guava:32.1.1-jre")
+
+    // testing
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // ui
+    implementation("com.miglayout:miglayout:3.7.4")
+    implementation("com.formdev:flatlaf:1.6.5")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+application {
+    mainClass.set("lab.four.Main")
 }
