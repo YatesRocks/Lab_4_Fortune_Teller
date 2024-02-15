e9b5820b138e723c26b74737a050151d954e78bd
YatesRocks <yates.j.a@outlook.com>
2024-02-15 17:17:44 -0500

    copy from lab_01

--- src/main/java/lab/four/view/MainView.java
+++ src/main/java/lab/four/view/MainView.java
@@ -1,8 +1,10 @@
 package lab.four.view;
 
 import lab.four.Main;
+import lab.four.controller.CardHandler;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainView extends JFrame {
     public MainView(int width, int height, String title, String path) {
@@ -14,6 +16,17 @@ public class MainView extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        initLayout();
+
         setVisible(true);
+    }
+
+    private void initLayout() {
+        CardLayout layout;
+        setLayout(layout = new CardLayout());
+        JPanel cardPanel = new JPanel(layout);
+        CardHandler.registerLayout(layout);
+        CardHandler.registerRootPanel(cardPanel);
+        add(cardPanel);
     }
 }
---
+++ src/main/java/lab/four/view/card/utils/TitleFont.java
@@ -0,0 +1,9 @@
+package lab.four.view.card.utils;
+
+import java.awt.*;
+
+public class TitleFont extends Font {
+    public TitleFont() {
+        super("serif", Font.BOLD, 64);
+    }
+}
